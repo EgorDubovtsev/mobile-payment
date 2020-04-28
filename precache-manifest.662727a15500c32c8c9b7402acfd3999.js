fed5ad140ed61492dd69ea3981d0d9f4b5460f56
Egor <egot.dubovczcev@yandex.ru>
2020-04-28 17:05:11 +0400

    Updates

---
+++ precache-manifest.662727a15500c32c8c9b7402acfd3999.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46668a3f08b57332e61c9a01066ff9ac",
+    "url": "/mobile-payment/index.html"
+  },
+  {
+    "revision": "1a84ef280ab2513e41c8",
+    "url": "/mobile-payment/static/css/main.3fd36bcc.chunk.css"
+  },
+  {
+    "revision": "73aa10cdca00c1486bed",
+    "url": "/mobile-payment/static/js/2.cb31c721.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mobile-payment/static/js/2.cb31c721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a84ef280ab2513e41c8",
+    "url": "/mobile-payment/static/js/main.84326099.chunk.js"
+  },
+  {
+    "revision": "1819dc7f7def6a5e492e",
+    "url": "/mobile-payment/static/js/runtime-main.4729195a.js"
+  },
+  {
+    "revision": "11972fb6f0148e9b63543feae7730297",
+    "url": "/mobile-payment/static/media/bg.11972fb6.jpg"
+  }
+]);
